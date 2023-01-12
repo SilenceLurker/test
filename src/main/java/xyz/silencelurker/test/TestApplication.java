4679eb65b4d131f26c8c884a0aeba483da50409d
Silence_Lurker <slvccans@gmail.com>
2023-01-12 09:58:28 +0000

    first commit

---
+++ src/main/java/xyz/silencelurker/test/TestApplication.java
@@ -0,0 +1,16 @@
+package xyz.silencelurker.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Silence_Lurker
+ */
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
